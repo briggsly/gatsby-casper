6c5772ea237f8d932a51864a494fe275725fbd61
briggsly <briggsbio@gmail.com>
2020-01-16 11:38:26 -0600

    Updated to Github pages thus configs updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 module.exports = {
   siteMetadata: {
-    title: 'Ghost',
-    description: 'The professional publishing platform',
-    siteUrl: 'https://gatsby-casper.netlify.com', // full path to blog - no ending slash
+    title: 'TomorrowScale',
+    description: 'How to Build the Future',
+    siteUrl: 'https://briggsly.github.io/gatsby-casper/', // full path to blog - no ending slash
   },
   mapping: {
     'MarkdownRemark.frontmatter.author': 'AuthorYaml',
@@ -47,7 +47,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-canonical-urls',
       options: {
-        siteUrl: 'https://gatsby-casper.netlify.com',
+        siteUrl: 'https://briggsly.github.io/gatsby-casper/',
       },
     },
     'gatsby-plugin-emotion',
--- src/website-config.ts
+++ src/website-config.ts
@@ -54,7 +54,7 @@ const config: WebsiteConfig = {
   coverImage: 'img/tomorrowscale-bg.jpg',
   logo: 'img/tomorrowscale-logo-white-250px.png',
   lang: 'en',
-  siteUrl: 'https://hungry-hypatia-c14335.netlify.com/',
+  siteUrl: 'https://briggsly.github.io/gatsby-casper/',
   twitter: 'https://twitter.com/tomorrowscale',
   showSubscribe: true,
   mailchimpAction: 'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
